003aed7de57fd0bfb8e726f08eb5bab047dfd69a
Dennis Quilantang <quildm@ymail.com>
2017-09-20 16:49:17 -0700

    created first commit

---
+++ src/app/supersaiyanthree/supersaiyanthree.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-supersaiyanthree',
+  templateUrl: './supersaiyanthree.component.html',
+  styleUrls: ['./supersaiyanthree.component.css']
+})
+export class SupersaiyanthreeComponent implements OnInit {
+  @Input() powerLevel;
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
